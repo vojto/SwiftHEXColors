623fa7b1c74df703ee169e0465e94f3d1238f6b1
Vojtech Rinik <vojto@rinik.net>
2016-11-16 09:45:40 +0100

    Creating 2 targets for iOS and macOS

---
+++ SwiftHEXColors/SwiftHEXColors.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHEXColors.h
+//  SwiftHEXColors
+//
+//  Created by Vojtech Rinik on 16/11/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftHEXColors.
+FOUNDATION_EXPORT double SwiftHEXColorsVersionNumber;
+
+//! Project version string for SwiftHEXColors.
+FOUNDATION_EXPORT const unsigned char SwiftHEXColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHEXColors/PublicHeader.h>
+
+
